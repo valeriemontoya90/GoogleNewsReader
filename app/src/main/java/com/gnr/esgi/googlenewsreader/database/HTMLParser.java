618a258878ac607d820d7e46d15fb2cdd443ccd1
Ismail NGUYEN <me@nguyenismail.com>
2015-12-16 23:16:21 +0100

    Add HtmlParser utility class

--- app/src/main/java/com/gnr/esgi/googlenewsreader/database/HTMLParser.java
+++ app/src/main/java/com/gnr/esgi/googlenewsreader/database/HTMLParser.java
@@ -3,8 +3,25 @@ package com.gnr.esgi.googlenewsreader.database;
 /**
  * Created by Ismail on 14-12-2015.
  */
-public class HTMLParser {
+
+public final class HTMLParser {
+    public static final String ELLIPSIS = "&ellip;";
+    public static final String TAG_PATTERN = "<[^>]+>";
+    public static final String WHITESPACE_PATTERN = "\\s!+";
+
+    public static String removeTags(String source) {
+        return source
+                .replaceAll(TAG_PATTERN, "")
+                .replaceAll(WHITESPACE_PATTERN, " ")
+                .trim();
+    }
+
     public String parse(String source) {
-        return source;
+        final int bodyIndex = source.indexOf("<body");
+
+        if(bodyIndex > -1)
+            source = source.substring(bodyIndex);
+
+        return removeTags(source);
     }
 }
