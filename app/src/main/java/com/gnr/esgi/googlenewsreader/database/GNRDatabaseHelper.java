5979c54c6e32027452b4ee84c565b2a9735d49c9
Valerie Montoya <valeriemontoya90@gmail.com>
2016-01-06 23:14:15 +0100

    add changes in GNRDatabaseHelper
    add new table called News with its create and drop function

--- app/src/main/java/com/gnr/esgi/googlenewsreader/database/GNRDatabaseHelper.java
+++ app/src/main/java/com/gnr/esgi/googlenewsreader/database/GNRDatabaseHelper.java
@@ -3,6 +3,7 @@ package com.gnr.esgi.googlenewsreader.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 import com.gnr.esgi.googlenewsreader.R;
 
@@ -17,12 +18,40 @@ public class GNRDatabaseHelper extends SQLiteOpenHelper {
         super(context, context.getString(R.string.app_settings_db_name), null, Integer.valueOf("1"));
     }
 
-    public void onCreate(SQLiteDatabase db) {
+    public final class NewsContract {
+        public NewsContract() {}
 
+        public abstract class NewEntry implements BaseColumns {
+            public static final String TABLE_NAME = "news";
+
+            public static final String COLUMN_TITLE = "new_title";
+            public static final String COLUMN_DATE = "new_date";
+            public static final String COLUMN_CONTENT = "new_content";
+            public static final String COLUMN_SOURCE = "new_source";
+        }
+    }
+
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String TEXT_TYPE = " TEXT";
+        String REAL_TYPE = " REAL";
+        String INTEGER_TYPE = " NUMERIC";
+        String COMMA_SEPARATOR = ",";
+
+        String CREATE_TABLE_NEWS =
+                "CREATE TABLE " + NewsContract.NewEntry.TABLE_NAME + " (" +
+                        NewsContract.NewEntry._ID + INTEGER_TYPE + " PRIMARY_KEY" + COMMA_SEPARATOR +
+                        NewsContract.NewEntry.COLUMN_TITLE + TEXT_TYPE + COMMA_SEPARATOR +
+                        NewsContract.NewEntry.COLUMN_DATE + TEXT_TYPE + COMMA_SEPARATOR +
+                        NewsContract.NewEntry.COLUMN_CONTENT + TEXT_TYPE + COMMA_SEPARATOR +
+                        NewsContract.NewEntry.COLUMN_SOURCE + TEXT_TYPE + " TEXT_TYPE" + ")";
+
+        sqLiteDatabase.execSQL(CREATE_TABLE_NEWS);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        String DROP_TABLE_NEWS = "DROP TABLE IF EXISTS " + NewsContract.NewEntry.TABLE_NAME;
+        sqLiteDatabase.execSQL(DROP_TABLE_NEWS);
+        onCreate(sqLiteDatabase);
     }
 }
