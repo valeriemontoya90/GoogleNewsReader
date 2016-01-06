aa2c728f4efbbef56c939b4da61e1821d5cb0cfb
Valerie Montoya <valeriemontoya90@gmail.com>
2016-01-06 23:46:27 +0100

    in GNRDatabaseHelper: renamed News table in Articles

--- app/src/main/java/com/gnr/esgi/googlenewsreader/database/GNRDatabaseHelper.java
+++ app/src/main/java/com/gnr/esgi/googlenewsreader/database/GNRDatabaseHelper.java
@@ -1,13 +1,11 @@
 package com.gnr.esgi.googlenewsreader.database;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 import com.gnr.esgi.googlenewsreader.R;
-import com.gnr.esgi.googlenewsreader.model.News;
 
 /**
  * Created by valerie on 06/01/16.
@@ -20,11 +18,11 @@ public class GNRDatabaseHelper extends SQLiteOpenHelper {
         super(context, context.getString(R.string.app_settings_db_name), null, Integer.valueOf("1"));
     }
 
-    public final class NewsContract {
-        public NewsContract() {}
+    public final class ArticleContract {
+        public ArticleContract() {}
 
-        public abstract class NewEntry implements BaseColumns {
-            public static final String TABLE_NAME = "news";
+        public abstract class ArticleEntry implements BaseColumns {
+            public static final String TABLE_NAME = "articles";
 
             public static final String COLUMN_TITLE = "new_title";
             public static final String COLUMN_DATE = "new_date";
@@ -40,39 +38,20 @@ public class GNRDatabaseHelper extends SQLiteOpenHelper {
         String COMMA_SEPARATOR = ",";
 
         String CREATE_TABLE_NEWS =
-                "CREATE TABLE " + NewsContract.NewEntry.TABLE_NAME + " (" +
-                        NewsContract.NewEntry._ID + INTEGER_TYPE + " PRIMARY_KEY" + COMMA_SEPARATOR +
-                        NewsContract.NewEntry.COLUMN_TITLE + TEXT_TYPE + COMMA_SEPARATOR +
-                        NewsContract.NewEntry.COLUMN_DATE + TEXT_TYPE + COMMA_SEPARATOR +
-                        NewsContract.NewEntry.COLUMN_CONTENT + TEXT_TYPE + COMMA_SEPARATOR +
-                        NewsContract.NewEntry.COLUMN_SOURCE + TEXT_TYPE + ")";
+                "CREATE TABLE " + ArticleContract.ArticleEntry.TABLE_NAME + " (" +
+                        ArticleContract.ArticleEntry._ID + INTEGER_TYPE + " PRIMARY_KEY" + COMMA_SEPARATOR +
+                        ArticleContract.ArticleEntry.COLUMN_TITLE + TEXT_TYPE + COMMA_SEPARATOR +
+                        ArticleContract.ArticleEntry.COLUMN_DATE + TEXT_TYPE + COMMA_SEPARATOR +
+                        ArticleContract.ArticleEntry.COLUMN_CONTENT + TEXT_TYPE + COMMA_SEPARATOR +
+                        ArticleContract.ArticleEntry.COLUMN_SOURCE + TEXT_TYPE + ")";
 
         sqLiteDatabase.execSQL(CREATE_TABLE_NEWS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        String DROP_TABLE_NEWS = "DROP TABLE IF EXISTS " + NewsContract.NewEntry.TABLE_NAME;
+        String DROP_TABLE_NEWS = "DROP TABLE IF EXISTS " + ArticleContract.ArticleEntry.TABLE_NAME;
         sqLiteDatabase.execSQL(DROP_TABLE_NEWS);
         onCreate(sqLiteDatabase);
-    }
-
-    ///////////////
-    ////NEWS//////
-    /////////////
-
-    public long addNew(News oneNew) {
-        ContentValues values = new ContentValues();
-        values.put(NewsContract.NewEntry.COLUMN_TITLE, oneNew.getTitle());
-        values.put(NewsContract.NewEntry.COLUMN_DATE, "date");
-        values.put(NewsContract.NewEntry.COLUMN_CONTENT, oneNew.getContent());
-
-        long newRowId = this.getWritableDatabase().insert(
-                NewsContract.NewEntry.TABLE_NAME,
-                null,
-                values
-        );
-
-        return newRowId;
     }
 }
