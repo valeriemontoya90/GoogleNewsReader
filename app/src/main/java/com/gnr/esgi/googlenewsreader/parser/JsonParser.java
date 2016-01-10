e7c4c9a6dcd39f9b1ea20035b497ca582ef85f11
Ismail NGUYEN <me@nguyenismail.com>
2016-01-10 02:29:32 +0100

    Fix empty picture news retrieving

--- app/src/main/java/com/gnr/esgi/googlenewsreader/parser/JsonParser.java
+++ app/src/main/java/com/gnr/esgi/googlenewsreader/parser/JsonParser.java
@@ -1,5 +1,7 @@
 package com.gnr.esgi.googlenewsreader.parser;
 
+import android.util.Log;
+
 import com.gnr.esgi.googlenewsreader.helper.NewsHelper;
 import com.gnr.esgi.googlenewsreader.model.News;
 import com.gnr.esgi.googlenewsreader.model.Source;
@@ -10,23 +12,31 @@ import java.util.List;
 public class JsonParser {
     public static List<News> parse(ArrayList<LinkedTreeMap<String, Object>> maps) {
         List<News> listNews = new ArrayList<>();
-        for(LinkedTreeMap<String, Object> map : maps) {
-            News news = new News();
 
-            news.setTitle((String) map.get(NewsHelper.KEY_TITLE));
-            news.setContent((String) map.get(NewsHelper.KEY_CONTENT));
+        try {
+            for(LinkedTreeMap<String, Object> map : maps) {
+                News news = new News();
 
-            //Parse date from string to Date
-            //news.setDate((String) map.get(NewsHelper.KEY_DATE));
+                news.setTitle((String) map.get(NewsHelper.KEY_TITLE));
+                news.setContent((String) map.get(NewsHelper.KEY_CONTENT));
 
-            news.setPicture((String) ((LinkedTreeMap<String, Object>) map.get("image")).get("url"));
+                //Parse date from string to Date
+                //news.setDate((String) map.get(NewsHelper.KEY_DATE));
 
-            Source source = new Source();
-            source.setName((String) map.get(NewsHelper.KEY_SOURCE));
-            source.setUrl((String) map.get(NewsHelper.KEY_LINK));
-            news.setSource(source);
+                LinkedTreeMap<String, Object> picture = (LinkedTreeMap<String, Object>) map.get(NewsHelper.KEY_PICTURE);
+                news.setPicture(picture != null
+                        ? (String) picture.get("url")
+                        : "");
 
-            listNews.add(news);
+                Source source = new Source();
+                source.setName((String) map.get(NewsHelper.KEY_SOURCE));
+                source.setUrl((String) map.get(NewsHelper.KEY_LINK));
+                news.setSource(source);
+
+                listNews.add(news);
+            }
+        } catch(Exception e) {
+            Log.w("JsonParser", "Error while reading news");
         }
 
         return listNews;
