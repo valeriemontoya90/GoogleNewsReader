a5a9be83d2a441d81dd8284a7546a075f579dc86
Valerie Montoya <valeriemontoya90@gmail.com>
2016-01-24 20:36:22 +0100

    create a Webservices folder and put it a Webservices class

---
+++ app/src/main/java/com/gnr/esgi/googlenewsreader/Webservices/Webservices.java
@@ -0,0 +1,18 @@
+package com.gnr.esgi.googlenewsreader.Webservices;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+/**
+ * Created by valerie on 24/01/16.
+ */
+public class Webservices {
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public Webservices() {
+    }
+
+    //get requests
+    public static void getArticlesByTag(AsyncHttpResponseHandler responseHandler) {
+    }
+}
